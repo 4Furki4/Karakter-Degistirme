0f0a889bf0e28086a85f98e62956b4a89b0a64ee
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-16 16:07:34 +0300

    Ilk mesajı yazdıran IlkMesaj ve girilen kelimeleri string dizi olarak döndüren KelimeleriAlma metotları oluşturuldu

--- Konsol_Islemleri.cs
+++ Konsol_Islemleri.cs
@@ -1,7 +1,16 @@
+using System;
 namespace KaraterDegistirme
 {
     public class Konsol_Islemleri
     {
-        
+        public void IlkMesaj()
+        {
+            System.Console.Write("Lütfen aralarında boşluk olacak şekilde kelimeler giriniz:");
+        }
+        public string[] KelimeleriAlma()
+        {
+            string input = Console.ReadLine();
+            return input.Split(" ");
+        }
     }
 }
