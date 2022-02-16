0aa90503480303bc1257ca8cfd79638f37ad27a0
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-16 16:19:40 +0300

    Kelimelerin son ve baş karakterlerini değiştiren YerleriDegistir metodu yazıldı

--- Karakter_Islemleri.cs
+++ Karakter_Islemleri.cs
@@ -1,7 +1,22 @@
+using System;
+using System.Collections.Generic;
 namespace KaraterDegistirme
 {
     public class Karakter_Islemleri
     {
+        public List<string> YerleriDegistir(string[] arr)
+        {
+            List<string> sonKelimeler= new List<string>();
+            foreach (var item in arr)
+            {
+                int uzunluk = item.Length;
+                string ilkKelime=arr[0]; 
+                item.Replace(arr[0],arr[uzunluk-1]);
+                item.Replace(arr[uzunluk-1],ilkKelime);
+                sonKelimeler.Add(item);
+            }
+            return sonKelimeler;
+        }
         
     }
 }
